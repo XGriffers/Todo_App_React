c094f8aaffb2f71ab18871c277e2e06439a53422
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-04-03 00:10:05 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo192 from './logo.svg';
 import './App.css';
 import ListItems from './ListItems'
 import { library } from '@fortawesome/fontawesome-svg-core'
