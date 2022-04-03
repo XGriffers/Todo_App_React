cc58a9e0ab04bf130ed8e1041eaccb5c6ddff258
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-04-03 00:53:59 -0400

    Hopefully this is fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo192 from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import ListItems from './ListItems'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -8,13 +8,13 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 library.add(faTrash)
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      items:[],
-      currentItem:{
-        text:'',
-        key:''
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
       }
     }
     this.addItem = this.addItem.bind(this);
@@ -22,67 +22,67 @@ class App extends React.Component {
     this.deleteItem = this.deleteItem.bind(this);
     this.setUpdate = this.setUpdate.bind(this);
   }
-  addItem(e){
+  addItem(e) {
     e.preventDefault();
     const newItem = this.state.currentItem;
-    if(newItem.text !==""){
+    if (newItem.text !== "") {
       const items = [...this.state.items, newItem];
-    this.setState({
-      items: items,
-      currentItem:{
-        text:'',
-        key:''
-      }
-    })
+      this.setState({
+        items: items,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
     }
   }
-  handleInput(e){
+  handleInput(e) {
     this.setState({
-      currentItem:{
+      currentItem: {
         text: e.target.value,
         key: Date.now()
       }
     })
   }
-  deleteItem(key){
-    const filteredItems= this.state.items.filter(item =>
-      item.key!==key);
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item =>
+      item.key !== key);
     this.setState({
       items: filteredItems
     })
 
   }
-  setUpdate(text,key){
-    console.log("items:"+this.state.items);
+  setUpdate(text, key) {
+    console.log("items:" + this.state.items);
     const items = this.state.items;
-    items.map(item => {      
-      if(item.key === key){
-        console.log(item.key +"    "+key)
-        item.text= text;
+    items.map(item => {
+      if (item.key === key) {
+        console.log(item.key + "    " + key)
+        item.text = text;
       }
     })
     this.setState({
       items: items
     })
-    
-   
+
+
   }
- render(){
-  return (
-    <div className="App">
-      <header>
-        <form id="to-do-form" onSubmit={this.addItem}>
-          <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
-          <button type="submit">Add</button>
-        </form>
-        <p>{this.state.items.text}</p>
-        
-          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate}/>
-        
-      </header>
-    </div>
-  );
- }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter task" value={this.state.currentItem.text} onChange={this.handleInput}></input>
+            <button type="submit">Add</button>
+          </form>
+          <p>{this.state.items.text}</p>
+
+          <ListItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate} />
+
+        </header>
+      </div>
+    );
+  }
 }
 
 
