b35029b9d14fa8b4326260e8122d2fcf3c0ba05b
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-04-03 00:12:00 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -55,8 +55,8 @@ class App extends React.Component {
   setUpdate(text,key){
     console.log("items:"+this.state.items);
     const items = this.state.items;
-    items.map(item=>{      
-      if(item.key===key){
+    items.map(item => {      
+      if(item.key === key){
         console.log(item.key +"    "+key)
         item.text= text;
       }
