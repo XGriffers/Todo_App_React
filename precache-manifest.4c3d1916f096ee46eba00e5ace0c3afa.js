2ba8b9ff04d5f8fa60384f6a6c88179550631d8a
Xgriffers <stevengriffith6@student.purdueglobal.edu>
2022-04-03 01:01:16 -0400

    Updates

---
+++ precache-manifest.4c3d1916f096ee46eba00e5ace0c3afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8fa56a443c5694ad20e7766883eb2a",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "4ea3aa29a334ddd9fbc7",
+    "url": "/To-Do-App-in-React/static/css/main.a46af78a.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/To-Do-App-in-React/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "4ea3aa29a334ddd9fbc7",
+    "url": "/To-Do-App-in-React/static/js/main.13dfc37e.chunk.js"
+  },
+  {
+    "revision": "759cf7b4cf17cf63d488",
+    "url": "/To-Do-App-in-React/static/js/runtime~main.60884708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
